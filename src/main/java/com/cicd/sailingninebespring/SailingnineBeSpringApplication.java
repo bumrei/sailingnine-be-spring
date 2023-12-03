0a2172de42d720b72dfee68b3dd3234d90194999
Denny <bumrei@gmail.com>
2023-12-03 23:08:29 +0900

    git ignore setting

---
+++ src/main/java/com/cicd/sailingninebespring/SailingnineBeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.sailingninebespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SailingnineBeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SailingnineBeSpringApplication.class, args);
+    }
+
+}
