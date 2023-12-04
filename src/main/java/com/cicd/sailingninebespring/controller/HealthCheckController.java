051ef05c088492f2e8cf08e24ab8b0152af539ef
Denny <bumrei@gmail.com>
2023-12-04 12:24:20 +0900

    active.profiles setting done

--- src/main/java/com/cicd/sailingninebespring/controller/HealthCheckController.java
+++ src/main/java/com/cicd/sailingninebespring/controller/HealthCheckController.java
@@ -1,24 +1,24 @@
 package com.cicd.sailingninebespring.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-
 @RestController
 @RequiredArgsConstructor
 public class HealthCheckController {
 
+    @Value("${test.test-value}")
+    private String valueTest;
+
     @GetMapping("/health/check")
     public ResponseEntity<String> healthCheck() {
 
-        String msg = "This api works well!";
-
-        HashMap<String, Object> result = new HashMap<>();
+        String msg = "This api works well!" + valueTest;
 
         HttpHeaders header = new HttpHeaders();
         header.add("Content-Type", "application/json; charset=UTF-8");
